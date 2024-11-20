b9b82fe587543dd23ec02d84ee9d0e896d80cb1f
Clemens <100091164+Clemens00505@users.noreply.github.com>
2024-11-20 10:25:32 +0100

    Added routes and deleted util-env

--- apps/data-api/src/app/app.module.ts
+++ apps/data-api/src/app/app.module.ts
@@ -3,19 +3,20 @@ import { BackendFeaturesMealModule } from '@avans-nx-workshop/backend/features';
 import { UsersModule } from '@avans-nx-workshop/backend/user';
 import { AuthModule } from '@avans-nx-workshop/backend/auth';
 import { MongooseModule } from '@nestjs/mongoose';
-import { environment } from '@avans-nx-workshop/shared/util-env';
+// import { environment } from '@avans-nx-workshop/shared/util-env';
 import { Logger } from '@nestjs/common';
 
 @Module({
     imports: [
         BackendFeaturesMealModule,
         AuthModule,
-        MongooseModule.forRoot(environment.MONGO_DB_CONNECTION_STRING, {
+        MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION_STRING, {
             connectionFactory: (connection) => {
                 connection.on('connected', () => {
                     // console.log('is connected');
                     Logger.verbose(
-                        `Mongoose db connected to ${environment.MONGO_DB_CONNECTION_STRING}`
+                        `Mongoose db connected to ${process.env.MONGO_DB_CONNECTION_STRING}`,
+                        'AppModule'
                     );
                 });
                 connection._events.connected();
