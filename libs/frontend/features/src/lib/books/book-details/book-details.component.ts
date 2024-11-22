d8e9a30830281677880386421ff8227b656d61c6
Clemens <100091164+Clemens00505@users.noreply.github.com>
2024-11-22 11:10:08 +0100

    Created files
    Component and service

---
+++ libs/frontend/features/src/lib/books/book-details/book-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'avans-nx-workshop-book-details',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './book-details.component.html',
+    styleUrl: './book-details.component.css'
+})
+export class BookDetailsComponent {}
---
+++ libs/frontend/features/src/lib/books/book-edit/book-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'avans-nx-workshop-book-edit',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './book-edit.component.html',
+    styleUrl: './book-edit.component.css'
+})
+export class BookEditComponent {}
