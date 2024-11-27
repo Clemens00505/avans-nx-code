fee281b12e635064f12d7d0cc0aee347b8e3a8ec
Clemens <100091164+Clemens00505@users.noreply.github.com>
2024-11-27 09:35:05 +0100

    Update book.interface.ts

--- libs/shared/api/src/lib/models/book.interface.ts
+++ libs/shared/api/src/lib/models/book.interface.ts
@@ -15,8 +15,10 @@ export interface IBook extends IEntity {
     description: string;
     author: string;
     publicationYear: number;
+    launguage: string;
+    publicator: string;
 }
 
-export type ICreateBook = Pick<IBook, 'coverUrl' | 'title' | 'genre' | 'description' | 'author' | 'publicationYear'>;
+export type ICreateBook = Pick<IBook, 'coverUrl' | 'title' | 'genre' | 'description' | 'author' | 'publicationYear' | 'launguage' | 'publicator'>;
 export type IUpdateBook = Partial<Omit<IBook, 'id'>>;
 export type IUpsertBook = IBook;
