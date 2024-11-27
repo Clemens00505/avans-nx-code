288dbe4480a8ee93ca6c2c763b17670b181cfc18
Clemens <100091164+Clemens00505@users.noreply.github.com>
2024-11-27 22:29:33 +0100

    Update environment.production.ts

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,8 +3,8 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
 
-    ROOT_DOMAIN_URL: 'https://gentle-grass-03aaa6503.5.azurestaticapps.net',
-    dataApiUrl: 'https://avans-nx-api-clemens-esagaeaqe7gdezhn.northeurope-01.azurewebsites.net',
+    ROOT_DOMAIN_URL: 'https://gentle-cliff-055362a03.5.azurestaticapps.net',
+    dataApiUrl: 'https://avans-nx-api-clemens-esagaeaqe7gdezhn.northeurope-01.azurewebsites.net/api',
 
     MONGO_DB_CONNECTION_STRING: 'mongodb+srv://boekbeheer:Test123@boekbeheer.hl7bf.mongodb.net/boekbeheer'
 };
