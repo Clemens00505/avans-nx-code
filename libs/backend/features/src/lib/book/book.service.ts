17b9714745cdf7195c433281bb7579e0b78c44c3
Clemens <100091164+Clemens00505@users.noreply.github.com>
2024-12-02 13:29:44 +0100

    Release (#21)
    * Development (#16) (#17)
    
    * Update nx.json
    
    * Feature/user (#15)
    
    * Added routes and deleted util-env
    
    * Update .gitignore
    
    * Revert "Added routes and deleted util-env"
    
    This reverts commit b9b82fe587543dd23ec02d84ee9d0e896d80cb1f.
    
    * User from db
    
    * User-Details created
    
    * Hotfix
    
    * Fix
    
    * Users - Edit - New - Delete
    
    * Update project.json
    
    * Update environment.production.ts
    
    * V0.2 (#20)
    
    * Development (#16)
    
    * Update nx.json
    
    * Feature/user (#15)
    
    * Added routes and deleted util-env
    
    * Update .gitignore
    
    * Revert "Added routes and deleted util-env"
    
    This reverts commit b9b82fe587543dd23ec02d84ee9d0e896d80cb1f.
    
    * User from db
    
    * User-Details created
    
    * Hotfix
    
    * Fix
    
    * Users - Edit - New - Delete
    
    * Development (#19)
    
    * Update nx.json
    
    * Feature/user (#15)
    
    * Added routes and deleted util-env
    
    * Update .gitignore
    
    * Revert "Added routes and deleted util-env"
    
    This reverts commit b9b82fe587543dd23ec02d84ee9d0e896d80cb1f.
    
    * User from db
    
    * User-Details created
    
    * Hotfix
    
    * Fix
    
    * Users - Edit - New - Delete
    
    * Update dashboard.component.html
    
    * Update BoekenBeheer.png
    
    * Some old data back
    
    * Feature/db books (#18)
    
    * Added routes and deleted util-env
    
    * Update .gitignore
    
    * Revert "Added routes and deleted util-env"
    
    This reverts commit b9b82fe587543dd23ec02d84ee9d0e896d80cb1f.
    
    * User from db
    
    * Created files
    
    Component and service
    
    * Books
    
    * Fixed some things
    
    * Get books from db
    
    * Book-details and link to edit
    
    * Update book.interface.ts
    
    * Update book-details.component.html
    
    * Book entiteit
    
    * Changes the location of books in backend

---
+++ libs/backend/features/src/lib/book/book.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Book, BookDocument } from './books.schema';
+import { CreateBookDto, UpdateBookDto } from '../../../../dto/src';
+
+@Injectable()
+export class BooksService {
+    private readonly logger: Logger = new Logger(BooksService.name);
+
+    constructor(
+        @InjectModel(Book.name) private bookModel: Model<BookDocument>
+    ) {}
+
+    async findAll(): Promise<Book[]> {
+        this.logger.log(`Finding all books`);
+        const items = await this.bookModel.find().exec();
+        return items;
+    }
+
+    async findOne(_id: string): Promise<Book | null> {
+        this.logger.log(`Finding book with id ${_id}`);
+        const item = await this.bookModel.findOne({ _id }).exec();
+        if (!item) {
+            this.logger.debug('Book not found');
+        }
+        return item;
+    }
+
+    async create(createBookDto: CreateBookDto): Promise<Book> {
+        this.logger.log(`Create book ${createBookDto.title}`);
+        const createdItem = new this.bookModel(createBookDto);
+        return createdItem.save();
+    }
+
+    async update(_id: string, updateBookDto: UpdateBookDto): Promise<Book | null> {
+        this.logger.log(`Update book ${updateBookDto.title}`);
+        return this.bookModel.findByIdAndUpdate(_id, updateBookDto, { new: true }).exec();
+    }
+
+    async remove(_id: string): Promise<Book | null> {
+        this.logger.log(`Remove book with id ${_id}`);
+        return this.bookModel.findByIdAndDelete(_id).exec();
+    }
+}
